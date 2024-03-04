a3ce925cbf6216bec74ca8766523c91155fcab1b
Braylin <braylinvasquez20@gmail.com>
2024-03-04 18:46:06 -0400

    Finalizando el modelo Vehiculos

---
+++ Shared/Models/Vehiculos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Vehiculos
+    {
+        [Key]
+        public int VehiculoId { get; set; }
+        [Required(ErrorMessage ="Este campo es requrido")]
+        public string? Descripcion { get; set; }
+        [Required(ErrorMessage = "Este campo es requrido")]
+        public DateTime? Fecha { get; set; }
+        [Required(ErrorMessage = "Este campo es requrido")]
+        public double Gastos { get; set; }
+
+        [ForeignKey("VehiculoId")]
+        public ICollection<VehiculosDetalle?> VehiculosDetalles = new List<VehiculosDetalle?>();
+    }
+}
