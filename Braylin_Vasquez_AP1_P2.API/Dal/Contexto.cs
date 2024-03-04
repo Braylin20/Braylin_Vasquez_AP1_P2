dcaafae50bce1d2f9b74640ac0fad9c15b3761cf
Braylin <braylinvasquez20@gmail.com>
2024-03-04 18:46:33 -0400

    Agregando el contexto

---
+++ Braylin_Vasquez_AP1_P2.API/Dal/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+
+namespace Braylin_Vasquez_AP1_P2.API.Dal;
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    public DbSet<Accesorios> Accesorios { get; set;}
+    public DbSet<Vehiculos> Vehiculos { get; set;}
+    public DbSet<VehiculosDetalle> VehiculosDetalle { get; set;}
+}
