b31136686de40f71e1a1bec648ff402773430f4e
Braylin <braylinvasquez20@gmail.com>
2024-03-04 18:46:22 -0400

    Finalizando el modelo VehiculosDetalle

---
+++ Shared/Models/VehiculosDetalle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class VehiculosDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+        [ForeignKey("Vehiculos")]
+        public int VehiculoId { get;  set; }
+        [ForeignKey("Accesorios")]
+        public int AccesorioId { get; set; }
+        [Required(ErrorMessage ="Este campo es requerido")]
+        public double Valor { get; set; }
+    }
+}
